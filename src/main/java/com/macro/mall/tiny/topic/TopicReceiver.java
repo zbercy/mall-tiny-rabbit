d00754ba3e41d478bb8ad34c12ad03a54ed78c26
bercy <3254426955@qq.com>
2023-08-22 14:35:23 +0800

    c = 16

--- src/main/java/com/macro/mall/tiny/topic/TopicReceiver.java
+++ src/main/java/com/macro/mall/tiny/topic/TopicReceiver.java
@@ -39,7 +39,7 @@ public class TopicReceiver {
 	}
 
 	public void receive(String in, int receiver){
-		int b = 9;
+		int c = 16;
 		StopWatch watch = new StopWatch();
 		watch.start();
 		LOGGER.info("instance {} [x] Received '{}'", receiver, in);
